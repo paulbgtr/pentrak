79aad74b43ae4452b88f6ca2f80fce3b160d1b40
paulbg <paulbg@hey.com>
2023-09-15 16:21:11 +0300

    Implement Navbar

---
+++ apps/frontend/src/components/Navbar.svelte
@@ -0,0 +1,8 @@
+<div class="navbar bg-base-100">
+  <div class="navbar-start">
+    <a class="text-xl normal-case btn btn-ghost" href="/">pentrak</a>
+  </div>
+  <div class="navbar-end">
+    <a class="btn btn-primary" href="/signup">Sign Up</a>
+  </div>
+</div>
