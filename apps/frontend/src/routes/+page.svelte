80623745130bbdbd56188cfb02c2c756a63221de
paulbg <paulbg@hey.com>
2023-09-15 16:48:07 +0300

    Design the index page with some dummy data

--- apps/frontend/src/routes/+page.svelte
+++ apps/frontend/src/routes/+page.svelte
@@ -1,6 +1,92 @@
+<script>
+  import Card from "../components/Card.svelte";
+
+  const trendingBooks = [
+    {
+      title: "Dummy Title 1",
+      description: "Dummy Description 1",
+      link: "/dummy-link-1",
+    },
+    {
+      title: "Dummy Title 2",
+      description: "Dummy Description 2",
+      link: "/dummy-link-2",
+    },
+    {
+      title: "Dummy Title 3",
+      description: "Dummy Description 3",
+      link: "/dummy-link-3",
+    },
+    {
+      title: "Dummy Title 4",
+      description: "Dummy Description 4",
+      link: "/dummy-link-4",
+    },
+    {
+      title: "Dummy Title 5",
+      description: "Dummy Description 5",
+      link: "/dummy-link-5",
+    },
+  ];
+
+  const newBooks = [
+    {
+      title: "Dummy Title 1",
+      description: "Dummy Description 1",
+      link: "/dummy-link-1",
+    },
+    {
+      title: "Dummy Title 2",
+      description: "Dummy Description 2",
+      link: "/dummy-link-2",
+    },
+    {
+      title: "Dummy Title 3",
+      description: "Dummy Description 3",
+      link: "/dummy-link-3",
+    },
+    {
+      title: "Dummy Title 4",
+      description: "Dummy Description 4",
+      link: "/dummy-link-4",
+    },
+    {
+      title: "Dummy Title 5",
+      description: "Dummy Description 5",
+      link: "/dummy-link-5",
+    },
+  ];
+</script>
+
 <svelte:head>
   <title>Home</title>
   <meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section>Hello World</section>
+<section>
+  <h2 class="mb-3 text-2xl font-bold">🔥 Trending</h2>
+
+  <div class="grid grid-cols-5 gap-3">
+    {#each trendingBooks as book}
+      <Card
+        title={book.title}
+        description={book.description}
+        link={book.link}
+      />
+    {/each}
+  </div>
+</section>
+
+<section class="mt-3">
+  <h2 class="mb-3 text-2xl font-bold">🌱 New Books</h2>
+
+  <div class="grid grid-cols-5 gap-3">
+    {#each newBooks as book}
+      <Card
+        title={book.title}
+        description={book.description}
+        link={book.link}
+      />
+    {/each}
+  </div>
+</section>
