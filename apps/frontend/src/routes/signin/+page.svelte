d8da9e076cd9e7af58ff2fb52b595160b5bfa8ad
paulbg <paulbg@hey.com>
2023-09-16 08:48:32 +0300

    Design the Sign In page

---
+++ apps/frontend/src/routes/signin/+page.svelte
@@ -0,0 +1,24 @@
+<section class="py-32">
+  <div class="card bg-base-100">
+    <div class="items-center text-center card-body">
+      <h2 class="center card-title">Sign In</h2>
+      <input
+        type="email"
+        required
+        placeholder="Your Email"
+        class="w-full max-w-xs input input-bordered"
+      />
+      <input
+        type="password"
+        required
+        placeholder="Your Password"
+        class="w-full max-w-xs input input-bordered"
+      />
+      <button class="w-full max-w-xs btn btn-primary">Submit</button>
+      <p class="mt-3 italic text-gray-500">
+        Don't have an account? <a class="link" href="/signup">Sign Up</a>
+      </p>
+      <div />
+    </div>
+  </div>
+</section>
