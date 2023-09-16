5aab976d6c3c575c43e06baf119c2b2b93c8759c
paulbg <paulbg@hey.com>
2023-09-16 10:56:44 +0300

    Add link to sign up page to navbar

--- apps/frontend/src/components/Navbar.svelte
+++ apps/frontend/src/components/Navbar.svelte
@@ -2,7 +2,8 @@
   <div class="navbar-start">
     <a class="text-xl normal-case btn btn-ghost" href="/">pentrak</a>
   </div>
-  <div class="navbar-end">
+  <div class="gap-2 navbar-end">
+    <a class="btn btn-primary btn-outline" href="/signin">Sign In</a>
     <a class="btn btn-primary" href="/signup">Sign Up</a>
   </div>
 </div>
