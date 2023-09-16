7632ae8aea479593493a2bf21114bd2d2c45cdeb
paulbg <paulbg@hey.com>
2023-09-16 09:01:08 +0300

    Add min-h-screen to layout
    This is made in order to keep Footer at the very bottom

--- apps/frontend/src/routes/+layout.svelte
+++ apps/frontend/src/routes/+layout.svelte
@@ -1,14 +1,19 @@
 <script>
   import "./tailwind.css";
   import Navbar from "../components/Navbar.svelte";
+  import Footer from "../components/Footer.svelte";
 </script>
 
 <div class="app">
-  <Navbar />
+  <div class="min-h-screen">
+    <Navbar />
 
-  <main
-    class="px-4 py-2 mx-auto sm:px-8 md:px-16 lg:px-32 xl:px-64 sm:py-4 md:py-6 lg:py-8 xl:py-10"
-  >
-    <slot />
-  </main>
+    <main
+      class="px-4 py-2 mx-auto sm:px-8 md:px-16 lg:px-32 xl:px-64 sm:py-4 md:py-6 lg:py-8 xl:py-10"
+    >
+      <slot />
+    </main>
+  </div>
+
+  <Footer />
 </div>
