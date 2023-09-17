1a2e0dae932740b0d03add9b22f0f786153be2ba
paulbg <paulbg@hey.com>
2023-09-17 10:14:56 +0300

    Add responsive menu to Navbar
    This menu copies the right menu on the index page.

--- apps/frontend/src/components/Navbar.svelte
+++ apps/frontend/src/components/Navbar.svelte
@@ -1,9 +1,41 @@
 <div class="navbar bg-base-100">
   <div class="navbar-start">
-    <a class="text-xl normal-case btn btn-ghost" href="/">pentrak</a>
+    <div class="dropdown">
+      <label tabindex="0" class="btn btn-ghost lg:hidden">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="w-5 h-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          ><path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 6h16M4 12h8m-8 6h16"
+          /></svg
+        >
+      </label>
+      <ul
+        tabindex="0"
+        class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+      >
+        <li>
+          <h5 class="font-bold">Popular Genres</h5>
+          <ul class="p-2">
+            <li><a>Fiction</a></li>
+            <li><a>Non-Fiction</a></li>
+            <li><a>Science Fiction</a></li>
+            <li><a>Fantasy</a></li>
+            <li><a>Mystery</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+    <a class="text-xl normal-case btn btn-ghost">daisyUI</a>
   </div>
   <div class="gap-2 navbar-end">
-    <a class="btn btn-primary btn-outline" href="/signin">Sign In</a>
-    <a class="btn btn-primary" href="/signup">Sign Up</a>
+    <a href="/signin" class="btn btn-primary btn-outline">Sign In</a>
+    <a href="/signup" class="btn btn-primary">Sign Up</a>
   </div>
 </div>
