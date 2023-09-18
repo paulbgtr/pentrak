a40b305124cc5b6526e759f751c2d079bdc99197
paulbg <paulbg@hey.com>
2023-09-18 20:15:38 +0300

    Implement Info component
    This component will allow users to enter decsription of their book,
    author and title.

---
+++ apps/frontend/src/components/Post/Info.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import { book } from "../../store/book";
+</script>
+
+<section class="mt-3">
+  <div class="mb-3">
+    <h2 class="text-xl font-bold">Info</h2>
+    <p>Write here information about your book.</p>
+  </div>
+
+  <div class="flex flex-col gap-2">
+    <input
+      type="text"
+      bind:value={$book.title}
+      placeholder="Title"
+      class="w-full join-item input input-bordered"
+    />
+    <input
+      type="text"
+      bind:value={$book.description}
+      placeholder="Description"
+      class="w-full join-item input input-bordered"
+    />
+    <input
+      type="text"
+      bind:value={$book.author}
+      placeholder="Author"
+      class="w-full join-item input input-bordered"
+    />
+  </div>
+</section>
