9e58a7f6811a26de030598cf228c67be2ab7e78c
paulbg <paulbg@hey.com>
2023-09-19 17:14:18 +0300

    Refactor post route
    As this route may become big, I decided to refactor it into another
    `Post` component which will also implement some logic to create a POST
    request to the API with the data.

---
+++ apps/frontend/src/components/Post/Post.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Links from "./Links.svelte";
+  import Info from "./Info.svelte";
+  import Image from "./Image.svelte";
+
+  const handlePost = async () => {
+    // todo
+  };
+</script>
+
+<section class="grid justify-center mt-10">
+  <Links />
+  <Info />
+  <Image />
+
+  <button class="mt-3 btn btn-primary">Submit</button>
+</section>
--- apps/frontend/src/routes/post/+page.svelte
+++ apps/frontend/src/routes/post/+page.svelte
@@ -1,19 +1,5 @@
 <script lang="ts">
-  import Links from "../../components/Post/Links.svelte";
-  import Info from "../../components/Post/Info.svelte";
-  import Image from "../../components/Post/Image.svelte";
-
-  import { book } from "../../store/book";
-
-  const handlePost = async () => {
-    // todo
-  };
+  import Post from "../../components/Post/Post.svelte";
 </script>
 
-<section class="grid justify-center mt-10">
-  <Links />
-  <Info />
-  <Image />
-
-  <button class="mt-3 btn btn-primary">Submit</button>
-</section>
+<Post />
