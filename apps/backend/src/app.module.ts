81c551c2cfc5df8797d7fb7bf6fb9f17dd2f39c2
paulbg <paulbg@hey.com>
2023-09-21 14:22:29 +0300

    Add MongooseModule to AppModule

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.DB_URI)],
 })
 export class AppModule {}
