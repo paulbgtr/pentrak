e85d63be5fdcac550627af0520f792de1929f79f
paulbg <paulbg@hey.com>
2023-09-21 14:54:36 +0300

    Implement booksController

--- apps/backend/src/books/books.module.ts
+++ apps/backend/src/books/books.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Book, BookSchema } from './book.schema';
+import { BooksController } from './books.controllers';
 import { BooksService } from './books.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Book.name, schema: BookSchema }]),
   ],
+  controllers: [BooksController],
   providers: [BooksService],
 })
 export class BooksModule {}
