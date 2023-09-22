0f06da10faf3fd726bdf25ceb4f296ebd700d04e
paulbg <paulbg@hey.com>
2023-09-22 13:06:41 +0300

    Implement Sign Up page

--- apps/frontend/src/routes/signup/+page.svelte
+++ apps/frontend/src/routes/signup/+page.svelte
@@ -1,30 +1,85 @@
+<script lang="ts">
+  import { supabase } from "../../utils/supabase";
+
+  let isError = false;
+
+  const createUser = async (email: string, password: string) => {
+    try {
+      const { data, error } = await supabase.auth.signUp({
+        email: "",
+        password: "",
+      });
+
+      if (error) throw error;
+
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const onSubmit = async (e: Event) => {
+    const formData = new FormData(e.target as HTMLFormElement);
+
+    const data: any = {};
+    for (let field of formData) {
+      const [key, value] = field;
+      data[key] = value;
+    }
+
+    try {
+      await createUser(data.email, data.password);
+    } catch (error) {
+      setTimeout(() => {
+        isError = true;
+      }, 5000);
+    }
+  };
+</script>
+
 <section class="py-32">
   <div class="card bg-base-100">
-    <div class="items-center text-center card-body">
+    <form
+      on:submit|preventDefault={onSubmit}
+      class="items-center text-center card-body"
+    >
       <h2 class="center card-title">Sign Up</h2>
       <input
-        type="email"
+        name="username"
+        type="text"
         required
         placeholder="Your Username"
         class="w-full max-w-xs input input-bordered"
       />
       <input
+        name="email"
         type="email"
         required
         placeholder="Your Email"
         class="w-full max-w-xs input input-bordered"
       />
       <input
+        name="password"
         type="password"
         required
         placeholder="Your Password"
         class="w-full max-w-xs input input-bordered"
       />
-      <button class="w-full max-w-xs btn btn-primary">Submit</button>
+      <button type="submit" class="w-full max-w-xs btn btn-primary"
+        >Submit</button
+      >
       <p class="mt-3 italic text-gray-500">
         Already have an account? <a class="link" href="/signin">Sign In</a>
       </p>
       <div />
-    </div>
+      {#if isError}
+        <p>
+          <span class="text-center text-red-500"
+            >Error: Something unexpected happened while signing up. Please try
+            again.</span
+          >
+        </p>
+      {/if}
+    </form>
   </div>
 </section>
