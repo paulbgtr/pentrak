4e7284cf15aa23b15f5c47bff6fe61af0deb4236
paulbg <paulbg@hey.com>
2023-09-25 11:05:03 +0300

    Implement sign in page
    This page works the same as the signUp page. But when isSuccess == true,
    it redirects user to the index page.

--- apps/frontend/src/routes/signin/+page.svelte
+++ apps/frontend/src/routes/signin/+page.svelte
@@ -1,14 +1,68 @@
+<script lang="ts">
+  import { supabase } from "../../utils/supabase";
+
+  let isError = false;
+  let isSuccess = false;
+
+  $: {
+    if (isSuccess) {
+      window.location.href = "/";
+    }
+  }
+
+  const signIn = async (email: string, password: string) => {
+    try {
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+
+      if (error) throw error;
+
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const onSubmit = async (e: Event) => {
+    const formData = new FormData(e.target as HTMLFormElement);
+
+    const data: any = {};
+    for (let field of formData) {
+      const [key, value] = field;
+      data[key] = value;
+    }
+
+    try {
+      const user = await signIn(data.email, data.password);
+
+      isSuccess = true;
+    } catch (error) {
+      console.log(error);
+      setTimeout(() => {
+        isError = true;
+      }, 5000);
+    }
+  };
+</script>
+
 <section class="py-32">
   <div class="card bg-base-100">
-    <div class="items-center text-center card-body">
+    <form
+      on:submit|preventDefault={onSubmit}
+      class="items-center text-center card-body"
+    >
       <h2 class="center card-title">Sign In</h2>
       <input
+        name="email"
         type="email"
         required
         placeholder="Your Email"
         class="w-full max-w-xs input input-bordered"
       />
       <input
+        name="password"
         type="password"
         required
         placeholder="Your Password"
@@ -19,6 +73,13 @@
         Don't have an account? <a class="link" href="/signup">Sign Up</a>
       </p>
       <div />
-    </div>
+      <p>
+        {#if isError}
+          <span class="text-center text-red-500"
+            >Error: Entered credentials are incorrect. Please try again.</span
+          >
+        {/if}
+      </p>
+    </form>
   </div>
 </section>
