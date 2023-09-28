6c0141d59ab341a8bf477fe32ac0451b4cd5bfc4
paulbg <paulbg@hey.com>
2023-09-28 18:22:15 +0300

    Implement posting a book and error handling
    - Implement fetching the API route for posting a new book.
    - Implement error handling that is handled by the isError variable which
      sets to `true` when something encountered, and lets the user know
      about it in the UI.

--- apps/frontend/src/pageComponents/postPage/Post/Post.svelte
+++ apps/frontend/src/pageComponents/postPage/Post/Post.svelte
@@ -2,11 +2,40 @@
   import Links from "./Links.svelte";
   import Info from "./Info.svelte";
   import Image from "./Image.svelte";
+  import { book } from "../../../store/book";
 
   let isSuccess = false;
+  let isError = false;
 
   const handlePost = async () => {
-    // todo
+    const url = "http://localhost:3000/books";
+    console.log($book);
+
+    try {
+      if ($book.title && $book.author && $book.description) {
+        const response = await fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify($book),
+        });
+
+        if (response.ok) {
+          isSuccess = true;
+          return response.json();
+        }
+
+        isError = true;
+        throw new Error("Error posting book!");
+      }
+
+      isError = true;
+      throw new Error("Incomplete book data!");
+    } catch (error) {
+      isError = true;
+      console.error(error);
+    }
   };
 </script>
 
@@ -28,6 +57,16 @@
     <Info />
     <Image />
 
-    <button class="mt-3 btn btn-primary">Submit</button>
+    <button
+      on:click={() => {
+        console.log(handlePost());
+      }}
+      class="mt-3 btn btn-primary">Submit</button
+    >
+    {#if isError}
+      <p class="mt-3 text-center text-error">
+        There was an error submitting your post. Please try again.
+      </p>
+    {/if}
   {/if}
 </section>
