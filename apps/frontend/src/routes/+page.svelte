a2926427ea0d441d868bf1130a2c7b177292925f
paulbg <paulbg@hey.com>
2023-09-29 11:41:07 +0300

    refactor(+page.svelte): remove hardcoded book data and fetch books from API to improve data accuracy and reusability feat(+page.svelte): add onMount lifecycle function to fetch new books from API when the page is mounted

--- apps/frontend/src/routes/+page.svelte
+++ apps/frontend/src/routes/+page.svelte
@@ -1,61 +1,36 @@
-<script>
+<script lang="ts">
   import Card from "../pageComponents/index/Card.svelte";
+  import { onMount } from "svelte";
+  import type { Book } from "../types/Book.ts";
 
-  const trendingBooks = [
-    {
-      title: "Dummy Title 1",
-      description: "Dummy Description 1",
-      link: "/dummy-link-1",
-    },
-    {
-      title: "Dummy Title 2",
-      description: "Dummy Description 2",
-      link: "/dummy-link-2",
-    },
-    {
-      title: "Dummy Title 3",
-      description: "Dummy Description 3",
-      link: "/dummy-link-3",
-    },
-    {
-      title: "Dummy Title 4",
-      description: "Dummy Description 4",
-      link: "/dummy-link-4",
-    },
-    {
-      title: "Dummy Title 5",
-      description: "Dummy Description 5",
-      link: "/dummy-link-5",
-    },
-  ];
+  let newBooks: Book[] = [];
 
-  const newBooks = [
-    {
-      title: "Dummy Title 1",
-      description: "Dummy Description 1",
-      link: "/dummy-link-1",
-    },
-    {
-      title: "Dummy Title 2",
-      description: "Dummy Description 2",
-      link: "/dummy-link-2",
-    },
-    {
-      title: "Dummy Title 3",
-      description: "Dummy Description 3",
-      link: "/dummy-link-3",
-    },
-    {
-      title: "Dummy Title 4",
-      description: "Dummy Description 4",
-      link: "/dummy-link-4",
-    },
-    {
-      title: "Dummy Title 5",
-      description: "Dummy Description 5",
-      link: "/dummy-link-5",
-    },
-  ];
+  const fetchBooks = async () => {
+    const url = "http://localhost:3000/books";
+
+    try {
+      const response = await fetch(url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleNewBooks = async () => {
+    const books = await fetchBooks();
+    newBooks = books;
+  };
+
+  onMount(() => {
+    handleNewBooks();
+  });
 </script>
 
 <svelte:head>
@@ -65,20 +40,6 @@
 
 <div class="grid lg:grid-cols-4 lg:divide-x">
   <section class="col-span-3">
-    <div>
-      <h2 class="mb-3 text-2xl font-bold">🔥 Trending</h2>
-
-      <div>
-        {#each trendingBooks as book}
-          <Card
-            title={book.title}
-            description={book.description}
-            link={book.link}
-          />
-        {/each}
-      </div>
-    </div>
-
     <div class="mt-3">
       <h2 class="mb-3 text-2xl font-bold">🌱 New</h2>
 
@@ -86,11 +47,12 @@
         <Card
           title={book.title}
           description={book.description}
-          link={book.link}
+          link={`/books/${book.title}`}
         />
       {/each}
     </div>
   </section>
+
   <section class="px-10">
     <ul class="hidden w-56 lg:menu bg-base-100 rounded-box">
       <h2 class="mb-3 text-xl font-bold">Popular Genres</h2>
