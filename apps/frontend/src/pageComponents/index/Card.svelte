5a1dbfe60f1b3eccb243b049b766409f8cb5093d
paulbg <paulbg@hey.com>
2023-10-02 16:43:16 +0300

    fix(Card.svelte): fix link href to include id parameter for proper routing feat(Card.svelte): add id prop to Card component to pass book id for routing

--- apps/frontend/src/pageComponents/index/Card.svelte
+++ apps/frontend/src/pageComponents/index/Card.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
   import Upvote from "../../components/Upvote/Upvote.svelte";
 
+  export let id: number;
   export let title: string;
   export let description: string;
-  export let link: string;
 </script>
 
 <div class="card bg-base-100">
   <div class="grid grid-cols-2 card-body">
-    <a href={`books${link}`} class="col-span-1">
+    <a href={`/books/${id}`} class="col-span-1">
       <h2 class="card-title">{title}</h2>
       <p>{description}</p>
     </a>
