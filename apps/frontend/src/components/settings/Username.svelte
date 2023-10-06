4b8762562f2061f4b6b5910fd8eb64227c0faba1
paulbg <paulbg@hey.com>
2023-10-06 13:54:47 +0300

    feat(settings): add Username component to the settings page
    feat(settings): add Username, Email, and Password components to the settings page for better user customization options

---
+++ apps/frontend/src/components/settings/Username.svelte
@@ -0,0 +1,11 @@
+<section>
+  <h2 class="text-xl font-bold">Username</h2>
+  <form action="">
+    <input
+      type="text"
+      placeholder="Type here"
+      class="w-full max-w-xs input input-bordered"
+    />
+    <orm />
+  </form>
+</section>
