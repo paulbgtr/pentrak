895e981e9929ca2d76034ac1230bd418017d83e3
paulbg <paulbg@hey.com>
2023-10-08 09:31:58 +0300

    feat(users): add user profile page component
    This commit adds a new Svelte component for the user profile page. The component receives a `data` prop which contains information about the user. If the user is found in the data, the component displays the user's avatar, username, headline, and about section. If the user is not found, a "User not found" message is displayed.

---
+++ apps/frontend/src/routes/users/[slug]/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  export let data;
+</script>
+
+{#if !data.user}
+  <p>User not found</p>
+{:else}
+  <section class="flex gap-5">
+    <div class="avatar">
+      <div
+        class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2"
+      >
+        <img src={data.user.image_url} alt={data.user.username} />
+      </div>
+    </div>
+    <div>
+      <h1 class="font-bold">{data.user.username}</h1>
+      <h5>{data.user.headline}</h5>
+    </div>
+  </section>
+  <section class="mt-5">
+    <h3 class="text-lg font-bold">About</h3>
+    <div class="max-w-xl p-3 mt-1 bg-primary rounded-xl">
+      <p>{data.user.about}</p>
+    </div>
+  </section>
+{/if}
